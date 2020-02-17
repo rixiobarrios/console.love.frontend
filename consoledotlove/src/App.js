000f0add8ca6921e34f720257c0619bb8765a26e
jnlsings <jnlsings@gmail.com>
2020-02-16 21:20:19 -0600

    Add axios get all profiles, API call success
    1. Add axios api call to ProfilesList.js, get successful
    2. Add MoviesList component to App.js to display, not successful

--- consoledotlove/src/App.js
+++ consoledotlove/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
-
+import ProfilesList from './components/ProfilesList';
 class App extends Component {
   render() {
-    return <div className="App">This is our app</div>;
+    return (
+      <div className="App">
+        <ProfilesList />
+      </div>
+    );
   }
 }
 
---
+++ consoledotlove/src/components/ProfilesList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { APIURL } from '../config.js';
+
+class ProfilesList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: [],
+      error: false
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`${APIURL}/profiles`)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.profiles.map(profile => (
+          <li key={profile._id}>
+            <Link to={`/profiles/${profile._id}`}>{profile.name}</Link>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default ProfilesList;
