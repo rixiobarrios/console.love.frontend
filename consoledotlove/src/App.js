2e59948fedabe39eb7da76a3af8581bb9665b08f
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-02-17 20:50:12 -0600

    Setup Match Component

--- consoledotlove/src/App.js
+++ consoledotlove/src/App.js
@@ -4,17 +4,24 @@ import ProfilesList from './components/ProfilesList';
 import Welcome from './components/Welcome';
 import Profile from './components/Profile';
 import Edit from './components/Edit';
+import Match from './components/Match';
 import { Link, Switch, Route } from 'react-router-dom';
 
 const App = () => (
 	<div>
-		<header>{/* <Link to="/profiles">All Profiles</Link> */}</header>
+		<header>
+			<nav>
+				<Link to="/profiles">Logo IMG</Link>
+			</nav>
+		</header>
+
 		<main>
 			<Switch>
 				<Route exact path="/" component={Welcome} />
 				<Route exact path="/profiles" component={ProfilesList} />
 				<Route exact path="/profiles/:id" component={Profile} />
 				<Route exact path="/profiles/:id/edit" component={Edit} />
+				<Route exact path="/profiles/:id/match" component={Match} />
 			</Switch>
 		</main>
 	</div>
---
+++ consoledotlove/src/components/Match.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Match extends Component {
+	render() {
+		return <div>ITS A MATCH</div>;
+	}
+}
+
+export default Match;
