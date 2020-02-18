d6e346395429a5bb7147c503d1eb96d26b5dbf82
jnlsings <jnlsings@gmail.com>
2020-02-18 09:30:11 -0600

    Updated text in Match.js

--- consoledotlove/src/components/Match.js
+++ consoledotlove/src/components/Match.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class Match extends Component {
-	render() {
-		return <div>ITS A MATCH</div>;
-	}
+  render() {
+    return (
+      <div>
+        <h1>It's a .match()!</h1>
+      </div>
+    );
+  }
 }
 
 export default Match;
