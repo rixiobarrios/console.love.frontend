732326bab5ab5f67fad8ff96d2c1fdded000e4bb
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-02-17 20:56:16 -0600

    Change App into a class

--- consoledotlove/src/App.js
+++ consoledotlove/src/App.js
@@ -7,24 +7,28 @@ import Edit from './components/Edit';
 import Match from './components/Match';
 import { Link, Switch, Route } from 'react-router-dom';
 
-const App = () => (
-	<div>
-		<header>
-			<nav>
-				<Link to="/profiles">Logo IMG</Link>
-			</nav>
-		</header>
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<header>
+					<nav>
+						<Link to="/profiles">Logo IMG</Link>
+					</nav>
+				</header>
 
-		<main>
-			<Switch>
-				<Route exact path="/" component={Welcome} />
-				<Route exact path="/profiles" component={ProfilesList} />
-				<Route exact path="/profiles/:id" component={Profile} />
-				<Route exact path="/profiles/:id/edit" component={Edit} />
-				<Route exact path="/profiles/:id/match" component={Match} />
-			</Switch>
-		</main>
-	</div>
-);
+				<main>
+					<Switch>
+						<Route exact path="/" component={Welcome} />
+						<Route exact path="/profiles" component={ProfilesList} />
+						<Route exact path="/profiles/:id" component={Profile} />
+						<Route exact path="/profiles/:id/edit" component={Edit} />
+						<Route exact path="/profiles/:id/match" component={Match} />
+					</Switch>
+				</main>
+			</div>
+		);
+	}
+}
 
 export default App;
