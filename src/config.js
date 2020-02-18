b334ed5dc31a6182eabd7b7498538a0174fedc1a
jnlsings <jnlsings@gmail.com>
2020-02-18 12:32:50 -0600

    Update config to /profiles

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const APIURL =
   window.location.hostname === 'localhost'
     ? 'http://localhost:4000'
-    : 'https://consoledotlove-backend.herokuapp.com/';
+    : 'https://consoledotlove-backend.herokuapp.com/profiles';
