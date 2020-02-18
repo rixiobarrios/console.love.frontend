b3c13e144eb9fb116079a32c52a791aff3e0e5f6
jnlsings <jnlsings@gmail.com>
2020-02-18 12:12:42 -0600

    Update config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const APIURL =
   window.location.hostname === 'localhost'
     ? 'http://localhost:4000'
-    : 'REPLACE WITH DEPLOYED API URL';
+    : 'https://consoledotlove-backend.herokuapp.com/';
