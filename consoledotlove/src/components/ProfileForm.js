ff0c35688789fce86a25a111077283f6271802ea
jnlsings <jnlsings@gmail.com>
2020-02-18 09:26:14 -0600

    Clean up text in ProfileForm

--- consoledotlove/src/components/ProfileForm.js
+++ consoledotlove/src/components/ProfileForm.js
@@ -1,69 +1,69 @@
 import React from 'react';
 
 const ProfileForm = ({ profile, handleSubmit, handleChange }) => (
-	<form onSubmit={handleSubmit}>
-		<label htmlFor="image">Link to Profile Pic</label>
-		<input
-			name="image"
-			placeholder="Profile Pic"
-			value={profile.image}
-			onChange={handleChange}
-			required
-			id="image"
-		/>
+  <form onSubmit={handleSubmit}>
+    <label htmlFor="image"></label>
+    <input
+      name="image"
+      placeholder="Profile Pic Link"
+      value={profile.image}
+      onChange={handleChange}
+      required
+      id="image"
+    />
 
-		<label htmlFor="name">Name</label>
-		<input
-			name="name"
-			placeholder="Profile Name"
-			value={profile.name}
-			onChange={handleChange}
-			required
-			id="name"
-		/>
+    <label htmlFor="name"></label>
+    <input
+      name="name"
+      placeholder="Name"
+      value={profile.name}
+      onChange={handleChange}
+      required
+      id="name"
+    />
 
-		<label htmlFor="Age">Age</label>
-		<input
-			name="age"
-			placeholder="Profile Age"
-			value={profile.age}
-			onChange={handleChange}
-			required
-			id="age"
-		/>
+    <label htmlFor="Age"></label>
+    <input
+      name="age"
+      placeholder="Age"
+      value={profile.age}
+      onChange={handleChange}
+      required
+      id="age"
+    />
 
-		<label htmlFor="location">Location</label>
-		<input
-			name="location"
-			placeholder="Profile Location"
-			value={profile.location}
-			onChange={handleChange}
-			required
-			id="location"
-		/>
+    <label htmlFor="location"></label>
+    <input
+      name="location"
+      placeholder="Location"
+      value={profile.location}
+      onChange={handleChange}
+      required
+      id="location"
+    />
 
-		<label htmlFor="about">About Me</label>
-		<input
-			name="about"
-			placeholder="Profile About Me"
-			value={profile.about}
-			onChange={handleChange}
-			required
-			id="about"
-		/>
+    <label htmlFor="about"></label>
+    <input
+      name="about"
+      placeholder="About Me"
+      value={profile.about}
+      onChange={handleChange}
+      required
+      id="about"
+    />
 
-		<label htmlFor="languages">Programming Languages</label>
-		<input
-			name="languages"
-			placeholder="Programming Languages"
-			value={profile.languages}
-			onChange={handleChange}
-			required
-			id="languages"
-		/>
+    <label htmlFor="languages"></label>
+    <input
+      name="languages"
+      placeholder="Programming Languages"
+      value={profile.languages}
+      onChange={handleChange}
+      required
+      id="languages"
+    />
 
-		<button type="submit">Submit</button>
-	</form>
+    <button type="submit">Submit</button>
+  </form>
 );
 
 export default ProfileForm;
