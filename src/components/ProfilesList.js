3ebcb58703eb0f69af5fd289b919317fef128469
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-02-19 11:12:04 -0600

    Create randomized list for profiles

--- src/components/ProfilesList.js
+++ src/components/ProfilesList.js
@@ -1,51 +1,58 @@
 import React, { Component } from 'react';
 import './ProfilesList.css';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { APIURL } from '../config.js';
+import _ from 'lodash';
 
 class ProfilesList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profiles: [],
-      error: false
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			profiles: [],
+			error: false
+		};
+	}
 
-  componentDidMount() {
-    fetch(`${APIURL}/profiles`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState({ profiles: data });
-      })
-      .catch(() => {
-        this.setState({ error: true });
-      });
-  }
+	randomizeData = data => {
+		let profileShuffle = _.shuffle(data);
+		return profileShuffle;
+	};
 
-  render() {
-    if (this.state.error) {
-      return <div>Sorry, there was a problem getting the profiles :/</div>;
-    }
+	componentDidMount() {
+		fetch(`${APIURL}/profiles`)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+				let shuffledData = this.randomizeData(data);
+				console.log(shuffledData);
+				this.setState({ profiles: shuffledData });
+			})
+			.catch(() => {
+				this.setState({ error: true });
+			});
+	}
 
-    if (this.state.profiles.length === 0) {
-      return <div>Loading...</div>;
-    }
+	render() {
+		if (this.state.error) {
+			return <div>Sorry, there was a problem getting the profiles :/</div>;
+		}
 
-    return (
-      <div className="profile-container">
-        {this.state.profiles.map(profile => (
-          <div key={profile._id}>
-            <Link to={`/profiles/${profile._id}`}>
-              <img src={profile.image} />
-            </Link>
-          </div>
-        ))}
-      </div>
-    );
-  }
+		if (this.state.profiles.length === 0) {
+			return <div>Loading...</div>;
+		}
+
+		return (
+			<div className="profile-container">
+				{this.state.profiles.map(profile => (
+					<div key={profile._id}>
+						<Link to={`/profiles/${profile._id}`}>
+							<img src={profile.image} alt={profile.name} />
+						</Link>
+					</div>
+				))}
+			</div>
+		);
+	}
 }
 
 export default ProfilesList;
