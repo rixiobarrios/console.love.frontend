af3057bd56a8d07163ae6013f4c9a66a434d4fe2
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-02-19 16:11:07 -0600

    rebase commit

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 class Match extends Component {
-  render() {
-    return (
-      <div>
-        <h1>It's a .match()!</h1>
-      </div>
-    );
-  }
+	matchAlgorithm = () => {
+		let match = _.sample([true, false]);
+		if (match) {
+			return <h1>It's a .match()!</h1>;
+		} else {
+			return (
+				<div>
+					<h1>Sorry, it's not meant to be</h1>
+					<h2>Debugging in process...</h2>
+				</div>
+			);
+		}
+	};
+
+	render() {
+		return <div>{this.matchAlgorithm()}</div>;
+	}
 }
 
 export default Match;
