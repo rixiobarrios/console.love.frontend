72bced5ebc952dfef3d7282b93ce5d87933b5eb2
jnlsings <jnlsings@gmail.com>
2020-02-18 20:37:18 -0600

    Axios => fetch in ProfilesList.js
    Also delete commented out axios call from Welcome.js

--- src/components/ProfilesList.js
+++ src/components/ProfilesList.js
@@ -5,39 +5,47 @@ import { Link } from 'react-router-dom';
 import { APIURL } from '../config.js';
 
 class ProfilesList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            profiles: [],
-            error: false
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: [],
+      error: false
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${APIURL}/profiles`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ profiles: data });
+      })
+      .catch(() => {
+        this.setState({ error: true });
+      });
+  }
+
+  render() {
+    if (this.state.error) {
+      return <div>Sorry, there was a problem getting the profiles :/</div>;
     }
 
-    componentDidMount() {
-        axios
-            .get(`${APIURL}/profiles`)
-            .then(response => {
-                console.log(response.data);
-                this.setState({ profiles: response.data });
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    if (this.state.profiles.length === 0) {
+      return <div>Loading...</div>;
     }
 
-    render() {
-        return (
-            <div className="profile-container">
-                {this.state.profiles.map(profile => (
-                    <div key={profile._id}>
-                        <Link to={`/profiles/${profile._id}`}>
-                            <img src={profile.image} />
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        );
-    }
+    return (
+      <div className="profile-container">
+        {this.state.profiles.map(profile => (
+          <div key={profile._id}>
+            <Link to={`/profiles/${profile._id}`}>
+              <img src={profile.image} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ProfilesList;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,90 +5,76 @@ import { APIURL } from '../config';
 import ProfileForm from './ProfileForm';
 
 class Welcome extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			profile: {
-				image: '',
-				name: '',
-				age: '',
-				location: '',
-				about: '',
-				languages: ''
-			},
-			createdId: null,
-			error: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: {
+        image: '',
+        name: '',
+        age: '',
+        location: '',
+        about: '',
+        languages: ''
+      },
+      createdId: null,
+      error: false
+    };
+  }
 
-	handleChange = evt => {
-		evt.persist();
-		this.setState({
-			profile: {
-				...this.state.profile,
-				[evt.target.name]: evt.target.value
-			}
-		});
-	};
+  handleChange = evt => {
+    evt.persist();
+    this.setState({
+      profile: {
+        ...this.state.profile,
+        [evt.target.name]: evt.target.value
+      }
+    });
+  };
 
-	handleSubmit = event => {
-		event.preventDefault();
-		console.log(this.state.profile);
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.profile);
 
-		fetch(`${APIURL}/profiles`, {
-			method: 'POST',
-			headers: {
-				'Content-type': 'application/json; charset=UTF-8'
-			},
-			body: JSON.stringify(this.state.profile)
-		})
-			.then(response => {
-				response.json();
-			})
-			.then(response => {
-				console.log(response);
-				this.setState({ createdId: true });
-				this.props.setGuest(false);
-			})
-			.catch(() => {
-				this.setState({ error: true });
-			});
-	};
+    fetch(`${APIURL}/profiles`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      },
+      body: JSON.stringify(this.state.profile)
+    })
+      .then(response => {
+        response.json();
+      })
+      .then(response => {
+        console.log(response);
+        this.setState({ createdId: true });
+        this.props.setGuest(false);
+      })
+      .catch(() => {
+        this.setState({ error: true });
+      });
+  };
 
-	render() {
-		console.log(this.state.profile._id);
-		const { createdId } = this.state;
-		console.log(createdId);
-		// if a new profile is created, redirect the user to the page with the new profile by id
-		if (createdId) {
-			return <Redirect to={`/profiles`} />;
-		}
+  render() {
+    console.log(this.state.profile._id);
+    const { createdId } = this.state;
+    console.log(createdId);
+    // if a new profile is created, redirect the user to the page with the new profile by id
+    if (createdId) {
+      return <Redirect to={`/profiles`} />;
+    }
 
-		return (
-			<div>
-				<ProfileForm
-					profile={this.state.profile}
-					handleChange={this.handleChange}
-					handleSubmit={this.handleSubmit}
-				/>
-				<Link to="/profiles">Guest</Link>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <ProfileForm
+          profile={this.state.profile}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <Link to="/profiles">Guest</Link>
+      </div>
+    );
+  }
 }
 
 export default Welcome;
-
-//   handleSubmit = evt => {
-//     evt.preventDefault();
-//     axios
-//       .post(`${APIURL}/profiles`, { profile: this.state.profile })
-//       .then(response => {
-//         console.log(response.data);
-//         console.log(this.state.profile);
-//         this.setState({ createdId: response.data._id });
-//       })
-//       .catch(() => {
-//         this.setState({ error: true });
-//       });
-//   };
