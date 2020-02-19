cfa3432247de973f2d843a07f94e78d2e8b929d6
jnlsings <jnlsings@gmail.com>
2020-02-19 14:56:54 -0600

    Put id back in img tag in ProfilesList

--- src/components/ProfilesList.js
+++ src/components/ProfilesList.js
@@ -7,73 +7,77 @@ import _ from 'lodash';
 let pNumber = 0;
 
 class ProfilesList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			profiles: [],
-			shownProfiles: [],
-			error: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: [],
+      shownProfiles: [],
+      error: false
+    };
+  }
 
-	// shuffles data
-	randomizeData = data => {
-		let profileShuffle = _.shuffle(data);
-		return profileShuffle;
-	};
-	// updates shownProfiles
-	profileDisplay = () => {
-		let tempArray = [];
-		if (pNumber <= this.state.profiles.length) {
-			for (let i = 0; i < pNumber; i++) {
-				tempArray = tempArray.concat(this.state.profiles[i]);
-			}
-			this.setState({ shownProfiles: tempArray });
-		}
-	};
-	// increments shown data number
-	showMore = () => {
-		pNumber += 10;
-		this.profileDisplay();
-	};
-	componentDidMount() {
-		fetch(`${APIURL}/profiles`)
-			.then(response => response.json())
-			.then(data => {
-				let shuffledData = this.randomizeData(data);
-				this.setState({ profiles: shuffledData });
-				this.showMore();
-			})
-			.catch(() => {
-				this.setState({ error: true });
-			});
-	}
+  // shuffles data
+  randomizeData = data => {
+    let profileShuffle = _.shuffle(data);
+    return profileShuffle;
+  };
+  // updates shownProfiles
+  profileDisplay = () => {
+    let tempArray = [];
+    if (pNumber <= this.state.profiles.length) {
+      for (let i = 0; i < pNumber; i++) {
+        tempArray = tempArray.concat(this.state.profiles[i]);
+      }
+      this.setState({ shownProfiles: tempArray });
+    }
+  };
+  // increments shown data number
+  showMore = () => {
+    pNumber += 10;
+    this.profileDisplay();
+  };
+  componentDidMount() {
+    fetch(`${APIURL}/profiles`)
+      .then(response => response.json())
+      .then(data => {
+        let shuffledData = this.randomizeData(data);
+        this.setState({ profiles: shuffledData });
+        this.showMore();
+      })
+      .catch(() => {
+        this.setState({ error: true });
+      });
+  }
 
-	render() {
-		if (this.state.error) {
-			return <div>Sorry, there was a problem getting the profiles :/</div>;
-		}
+  render() {
+    if (this.state.error) {
+      return <div>Sorry, there was a problem getting the profiles :/</div>;
+    }
 
-		if (this.state.profiles.length === 0) {
-			return <div>Loading...</div>;
-		}
-		return (
-			<div>
-				<div className="profile-container">
-					{this.state.shownProfiles.map(profile => (
-						<div key={profile._id}>
-							<Link to={`/profiles/${profile._id}`}>
-								<img src={profile.image} alt={profile.name} />
-							</Link>
-						</div>
-					))}
-				</div>
-				{pNumber < this.state.profiles.length && (
-					<button onClick={this.showMore}>Show More</button>
-				)}
-			</div>
-		);
-	}
+    if (this.state.profiles.length === 0) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <div className="profile-container">
+          {this.state.shownProfiles.map(profile => (
+            <div key={profile._id}>
+              <Link to={`/profiles/${profile._id}`}>
+                <img
+                  id="profile-image-list"
+                  src={profile.image}
+                  alt={profile.name}
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+        {pNumber < this.state.profiles.length && (
+          <button onClick={this.showMore}>Show More</button>
+        )}
+      </div>
+    );
+  }
 }
 
 export default ProfilesList;
