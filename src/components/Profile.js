b080162e86283af3a010c9d1df4e4105d04f78d5
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-02-19 14:29:14 -0600

    Add CSS to progiles and single profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,54 +5,64 @@ import { Link } from 'react-router-dom';
 import { APIURL } from '../config';
 
 class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profile: null,
-      error: false
-    };
-  }
-
-  componentDidMount() {
-    fetch(`${APIURL}/profiles/${this.props.match.params.id}`)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ profile: data });
-      })
-      .catch(() => {
-        this.setState({ error: true });
-      });
-  }
-
-  render() {
-    if (this.state.error) {
-      return <div>Sorry, there was a problem getting the profile</div>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            profile: null,
+            error: false
+        };
     }
 
-    if (!this.state.profile) {
-      return <div>Loading...</div>;
+    componentDidMount() {
+        fetch(`${APIURL}/profiles/${this.props.match.params.id}`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ profile: data });
+            })
+            .catch(() => {
+                this.setState({ error: true });
+            });
     }
 
-    return (
-      <div className="profile-container">
-        <div className="profile-image">
-          <img src={this.state.profile.image} alt={this.state.profile.name} />
-        </div>
-        <div>
-          <ul>
-            <li>{this.state.profile.name}</li>
-            <li>{this.state.profile.age}</li>
-            <li>{this.state.profile.location}</li>
-            <li>{this.state.profile.bio}</li>
-            <li>{this.state.profile.languages}</li>
-            <li>{this.state.profile.github}</li>
-          </ul>
-          <Link to={`/profiles/${this.state.profile._id}/edit`}>Edit</Link>
-          <Link to={`/profiles/${this.state.profile._id}/match`}>Match</Link>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        if (this.state.error) {
+            return (
+                <div className="sorry">
+                    Sorry, there was a problem getting the profile
+                </div>
+            );
+        }
+
+        if (!this.state.profile) {
+            return <div className="loading">Loading...</div>;
+        }
+
+        return (
+            <div className="profile-box">
+                <img
+                    id="profile-image"
+                    src={this.state.profile.image}
+                    alt={this.state.profile.name}
+                />
+                <ul>
+                    <li>Name: {this.state.profile.name}</li>
+                    <li>Age: {this.state.profile.age}</li>
+                    <li>City: {this.state.profile.location}</li>
+                    <li>Bio: {this.state.profile.bio}</li>
+                    <li>Programming: {this.state.profile.languages}</li>
+                    <li>Github: {this.state.profile.github}</li>
+                </ul>
+                <div>
+                    <Link to={`/profiles/${this.state.profile._id}/edit`}>
+                        Edit
+                    </Link>
+                    <Link to={`/profiles/${this.state.profile._id}/match`}>
+                        Match
+                    </Link>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Profile;
--- src/components/ProfilesList.js
+++ src/components/ProfilesList.js
@@ -5,47 +5,53 @@ import { Link } from 'react-router-dom';
 import { APIURL } from '../config.js';
 
 class ProfilesList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profiles: [],
-      error: false
-    };
-  }
-
-  componentDidMount() {
-    fetch(`${APIURL}/profiles`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState({ profiles: data });
-      })
-      .catch(() => {
-        this.setState({ error: true });
-      });
-  }
-
-  render() {
-    if (this.state.error) {
-      return <div>Sorry, there was a problem getting the profiles :/</div>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            profiles: [],
+            error: false
+        };
     }
 
-    if (this.state.profiles.length === 0) {
-      return <div>Loading...</div>;
+    componentDidMount() {
+        fetch(`${APIURL}/profiles`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({ profiles: data });
+            })
+            .catch(() => {
+                this.setState({ error: true });
+            });
     }
 
-    return (
-      <div className="profile-container">
-        {this.state.profiles.map(profile => (
-          <div key={profile._id}>
-            <Link to={`/profiles/${profile._id}`}>
-              <img src={profile.image} />
-            </Link>
-          </div>
-        ))}
-      </div>
-    );
-  }
+    render() {
+        if (this.state.error) {
+            return (
+                <div>Sorry, there was a problem getting the profiles :/</div>
+            );
+        }
+
+        if (this.state.profiles.length === 0) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div className="profile-container">
+                {this.state.profiles.map(profile => (
+                    <div key={profile._id}>
+                        <Link to={`/profiles/${profile._id}`}>
+                            <img
+                                id="profile-image-list"
+                                src={profile.image}
+                                alt={profile.name}
+                            />
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default ProfilesList;
