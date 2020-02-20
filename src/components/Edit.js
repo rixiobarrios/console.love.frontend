863abf23ad127c1199c049dad1072033e9fb86f8
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-02-19 23:06:28 -0600

    Create border for tests/button setup/CSS setup

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -4,107 +4,110 @@ import { APIURL } from '../config';
 import ProfileForm from './ProfileForm';
 
 class Edit extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profile: {
-        image: '',
-        name: '',
-        age: '',
-        location: '',
-        bio: '',
-        languages: '',
-        github: ''
-      },
-      updated: false,
-      deleted: false,
-      error: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            profile: {
+                image: '',
+                name: '',
+                age: '',
+                location: '',
+                bio: '',
+                languages: '',
+                github: ''
+            },
+            updated: false,
+            deleted: false,
+            error: false
+        };
+    }
+
+    // get profile by id and set state with its values
+    componentDidMount() {
+        fetch(`${APIURL}/profiles/${this.props.match.params.id}`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ profile: data });
+            })
+            .catch(() => {
+                this.setState({ error: true });
+            });
+    }
+
+    // pre-populate edit form w/new state object properties
+    handleChange = evt => {
+        evt.persist();
+        this.setState({
+            profile: {
+                ...this.state.profile,
+                [evt.target.name]: evt.target.value
+            }
+        });
     };
-  }
 
-  // get profile by id and set state with its values
-  componentDidMount() {
-    fetch(`${APIURL}/profiles/${this.props.match.params.id}`)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ profile: data });
-      })
-      .catch(() => {
-        this.setState({ error: true });
-      });
-  }
+    handleSubmit = event => {
+        event.preventDefault();
 
-  // pre-populate edit form w/new state object properties
-  handleChange = evt => {
-    evt.persist();
-    this.setState({
-      profile: {
-        ...this.state.profile,
-        [evt.target.name]: evt.target.value
-      }
-    });
-  };
+        fetch(`${APIURL}/profiles/${this.props.match.params.id}/edit`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify(this.state.profile)
+        })
+            .then(response => {
+                this.setState({ updated: true });
+            })
+            .catch(() => {
+                this.setState({ error: true });
+            });
+    };
 
-  handleSubmit = event => {
-    event.preventDefault();
+    handleDelete = evt => {
+        fetch(`${APIURL}/profiles/${this.props.match.params.id}`, {
+            method: 'DELETE'
+        })
+            .then(res => {
+                this.setState({ deleted: true });
+            })
+            .catch(() => {
+                this.setState({ error: true });
+            });
+    };
 
-    fetch(`${APIURL}/profiles/${this.props.match.params.id}/edit`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8'
-      },
-      body: JSON.stringify(this.state.profile)
-    })
-      .then(response => {
-        this.setState({ updated: true });
-      })
-      .catch(() => {
-        this.setState({ error: true });
-      });
-  };
+    render() {
+        if (this.state.error) {
+            return <div className="error">There was an error :/</div>;
+        }
 
-  handleDelete = evt => {
-    fetch(`${APIURL}/profiles/${this.props.match.params.id}`, {
-      method: 'DELETE'
-    })
-      .then(res => {
-        this.setState({ deleted: true });
-      })
-      .catch(() => {
-        this.setState({ error: true });
-      });
-  };
+        if (this.state.updated) {
+            return <Redirect to={`/profiles/${this.props.match.params.id}`} />;
+        }
 
-  render() {
-    if (this.state.error) {
-      return <div>There was an error :/</div>;
+        if (this.state.deleted) {
+            return <Redirect to="/" />;
+        }
+
+        return (
+            <div>
+                <div>
+                    <ProfileForm
+                        profile={this.state.profile}
+                        handleChange={this.handleChange}
+                        handleSubmit={this.handleSubmit}
+                    />
+                </div>
+                <div className="delete-container">
+                    <button
+                        className="delete-button"
+                        onClick={this.handleDelete}
+                    >
+                        Delete Profile
+                    </button>
+                </div>
+            </div>
+        );
     }
-
-    if (this.state.updated) {
-      return <Redirect to={`/profiles/${this.props.match.params.id}`} />;
-    }
-
-    if (this.state.deleted) {
-      return <Redirect to="/" />;
-    }
-
-    return (
-      <div>
-        <div>
-          <ProfileForm
-            profile={this.state.profile}
-            handleChange={this.handleChange}
-            handleSubmit={this.handleSubmit}
-          />
-        </div>
-        <div className="delete-container">
-          <button className="delete-button" onClick={this.handleDelete}>
-            Delete Profile
-          </button>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default Edit;
