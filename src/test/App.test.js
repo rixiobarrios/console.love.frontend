c1c673f727e520fc63283c61b564ba48cfe1866f
jnlsings <jnlsings@gmail.com>
2020-02-20 15:18:34 -0600

    Setup App.test

---
+++ src/test/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../App';
+
+describe('App component', () => {
+  it('should render', () => {
+    const component = shallow(<App />);
+    console.log(component.find('.match-container'));
+    expect(component.find('.match-container').length).toBe(1);
+  });
+});
