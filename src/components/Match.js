0d10e43e3b0558e104a234b9cd53647dcf360b13
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-02-20 14:57:05 -0600

    Create testing for match

--- src/components/Match.js
+++ src/components/Match.js
@@ -2,23 +2,21 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 
 class Match extends Component {
-    matchAlgorithm = () => {
-        let match = _.sample([true, false]);
-        if (match) {
-            return <h1 className="match">It's a .match()!</h1>;
-        } else {
-            return (
-                <div className="not-match">
-                    <h1>Sorry, it's not meant to be</h1>
-                    <h2>Debugging in process...</h2>
-                </div>
-            );
-        }
-    };
+	matchAlgorithm = () => {
+		let match = _.sample([true, false]);
+		return match ? (
+			<h1 className="match">It's a .match()!</h1>
+		) : (
+			<div className="not-match">
+				<h1>Sorry, it's not meant to be</h1>
+				<h2>Debugging in process...</h2>
+			</div>
+		);
+	};
 
-    render() {
-        return <div>{this.matchAlgorithm()}</div>;
-    }
+	render() {
+		return <div className="match-container">{this.matchAlgorithm()}</div>;
+	}
 }
 
 export default Match;
---
+++ src/test/Match.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Match from '../components/Match';
+
+describe('Match component', () => {
+	it('should render', () => {
+		const component = shallow(<Match />);
+		console.log(component.find('.match-container'));
+		expect(component.find('.match-container').length).toBe(1);
+	});
+});
